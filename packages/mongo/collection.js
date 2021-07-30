4a7f4b5612f8f5553df0ff5a01d3bc7b9291a58b
filipenevola <filipenevola@gmail.com>
2021-07-30 16:59:37 -0400

    Fixes test affected by internal API changes on MongoInternals.defaultRemoteCollectionDriver Removing console.log

--- packages/mongo/collection.js
+++ packages/mongo/collection.js
@@ -42,7 +42,6 @@ Mongo.Collection = function Collection(name, optionsParam = {}) {
   }
 
   const collectionInstance = getCollectionInstanceOrNull({name, isAsync: optionsParam.isAsync});
-  console.log(`collectionInstance for ${name}, isAsync=${optionsParam.isAsync} already exists?`, !!collectionInstance);
 
   if (collectionInstance) {
     return collectionInstance;
